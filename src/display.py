b42a92f39201adb8bf05930da89396950ff87fb4
Ahmiere Davis <20143543@tafe.wa.edu.au>
2025-11-12 14:32:46 +0800

    Fixed issues identified by display unit tests

--- src/display.py
+++ src/display.py
@@ -9,4 +9,5 @@ class Display:
 
     def update(self, data):
         for key, value in data.items():
-            print(f"{key}: {value}")
+            setattr(self, key, value)
+
--- src/main.py
+++ src/main.py
@@ -1,47 +1,17 @@
-from sensor import Sensor
-from display import Display
+import carpark
+from carpark import CarPark
 
+class Display:
+    def __init__(self, id, message="", is_on=False, car_park=None):
+        self.id = id
+        self.message = message
+        self.is_on = is_on
+        self.car_park = carpark.CarPark
 
-class CarPark:
-    def __init__(self, location, capacity, plates=None, displays=None, sensors=None):
-        self.location = location
-        self.capacity = capacity
-        self.plates = plates or []
-        self.displays = displays or []
-        self.sensors = sensors or []
 
     def __str__(self):
-        return f"Car is parked at {self.location}, with {self.capacity} bays available."
+        return f"Display {self. id}: {self.message}"
 
-    def register(self, component):
-        if not isinstance(component, (Sensor, Display)):
-            raise TypeError("Object must be a Sensor or Display")
-        if isinstance(component, Sensor):
-            self.sensors.append(component)
-        elif isinstance(component, Display):
-            self.displays.append(component)
-
-    def update_displays(self):
-        data = {"available_bays": self.available_bays, "temperature": 25}
-        for display in self.displays:
-            display.update(data)
-
-    def add_car(self, plate):
-        self.plates.append(plate)
-        self.update_displays()
-
-    def remove_car(self, plate):
-        if plate in self.plates:
-            self.plates.remove(plate)
-            self.update_displays()
-        else:
-            raise ValueError(f"Plate {plate} not found — cannot remove")
-
-    @property
-    def available_bays(self):
-        result = self.capacity - len(self.plates)
-        if (result < 0):
-            return 0
-        else:
-            return result
-
+    def update(self, data):
+        for key, value in data.items():
+            print(f"{key}: {value}")
